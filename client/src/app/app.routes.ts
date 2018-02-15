43e9dfe5ba009e080b8014d19b62e324dcfeb60b
blake376 <blake376@morris.umn.edu>
2018-02-15 15:16:06 -0600

    fixed missing TodoListComponent reference

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import {ModuleWithProviders} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {UserListComponent} from './users/user-list.component';
+import {TodoListComponent} from './todos/todo-list.component';
 
 // Route Configuration
 export const routes: Routes = [
